c7005f5731dacf959bf9a5a07e3e0f94f3dc493d
Bruno Rogério <b.rogerio2007@gmail.com>
2022-01-11 22:29:15 -0400

    Iniciando aprendizado JPA Hibernate

--- src/teste/basico/NovoUsuario.java
+++ src/teste/basico/NovoUsuario.java
@@ -1,4 +1,18 @@
 package teste.basico;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class NovoUsuario {
+
+    public static void main(String[] args) {
+
+        //criando gerente de entidades
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+
+    }
+
 }
