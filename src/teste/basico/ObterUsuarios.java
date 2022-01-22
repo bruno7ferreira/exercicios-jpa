476b2a04a5dde33eb6451f606cab1f3f880cfea0
Bruno Rogério <b.rogerio2007@gmail.com>
2022-01-21 23:56:02 -0400

    Criando e consultando usuário usando JPA.

--- src/teste/basico/ObterUsuarios.java
+++ src/teste/basico/ObterUsuarios.java
@@ -1,8 +1,12 @@
 package teste.basico;
 
+import modelo.basico.Usuario;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class ObterUsuarios {
 
@@ -12,7 +16,21 @@ public class ObterUsuarios {
                 Persistence.createEntityManagerFactory("exercicios-jpa");
         EntityManager gerenteEntidade = fabricaGerenteEntidade.createEntityManager();
 
+        String jpql = "select u from Usuario u";
+        //consulta por TIPO de classe
+        TypedQuery<Usuario> query = gerenteEntidade.createQuery(jpql,Usuario.class);
+        //quantos resultados maximos quer ter
+        query.setMaxResults(5);
+        //vai ter o resultado e jogar dentro de "usuarios/lista"
+        List<Usuario>usuarios = query.getResultList();
 
+        for (Usuario usuario:usuarios){
+            System.out.println("ID: "+usuario.getId());
+        }
+
+
+        gerenteEntidade.close();
+        fabricaGerenteEntidade.close();
     }
 
 }
