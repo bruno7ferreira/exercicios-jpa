c180880183fe56c8e497f5f4629436997d86df33
Bruno Rogério <b.rogerio2007@gmail.com>
2022-12-03 21:29:10 -0400

    Voltando ao Ritmo

--- src/teste/basico/RecuperandoPratica.java
+++ src/teste/basico/RecuperandoPratica.java
@@ -1,7 +1,17 @@
 package teste.basico;
 
+import java.util.Scanner;
+
 public class RecuperandoPratica {
 
+    public static void main(String[] args) {
 
+        String nome;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("");
+
+
+    }
 
 }
